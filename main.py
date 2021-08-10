fdf7aae3eedbb7a0985f13e737ba982e77e24744
Matheus Girardin <7042521-mgirardin.inoa@users.noreply.gitlab.com>
2021-08-10 03:09:05 -0300

    style: rename controller files

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import json 
 from constants import http_response as http
 from security import cors
-from controllers.ArticleController import ArticleController, ArticlesController
-from controllers.AdminController import EmployeeSigninController, EmployeeRefreshController
-from controllers.SubscriberController import SubscriberController
-from controllers.ContactController import ContactController
+from controllers.article_controller import ArticleController, ArticlesController
+from controllers.admin_controller import EmployeeSigninController, EmployeeRefreshController
+from controllers.subscriber_controller import SubscriberController
+from controllers.contact_controller import ContactController
 
 def routes():
     routes = {
@@ -19,23 +19,27 @@ def routes():
 
 def router(request):
     if request.method == 'OPTIONS':
-        headers, allowed = cors.write_headers(request.headers.get("origin"))
-        if(allowed):
-            return ('', 204, headers)
-        return ('', 403, headers)
+        _handle_options_request(request)
     if(request.path not in routes()):
         return json.dumps({"error" : "Path {} not Found".format(request.path)}), 404, http.DEFAULT_HEADERS
-    tmp_router = routes()[request.path]
-    tmp_object = tmp_router()
-    response = {}
-    if(request.method.lower() not in dir(tmp_object)):
+    
+    route = routes()[request.path]
+    controller = route()
+    if(request.method.lower() not in dir(controller)):
         return http.METHOD_NOT_ALLOWED
     if(request.method.upper() == "GET"):
-        response, status, headers = tmp_object.get(request)
+        response, status, headers = controller.get(request)
     elif(request.method.upper() == "POST"):
-        response, status, headers = tmp_object.post(request)
+        response, status, headers = controller.post(request)
     else:
         return http.METHOD_NOT_ALLOWED
+    
     cors_headers, _ = cors.write_headers(request.headers.get("origin"))
     headers.update(cors_headers)
     return response, status, headers
+
+def _handle_options_request(request):
+    headers, allowed = cors.write_headers(request.headers.get("origin"))
+    if(allowed):
+        return ('', 204, headers)
+    return ('', 403, headers)
