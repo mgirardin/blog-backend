218897c874ab0fae9c5cd6fa3f181216430871c9
Matheus Girardin <7042521-mgirardin.inoa@users.noreply.gitlab.com>
2021-08-10 03:56:51 -0300

    fix: import

--- messages/comment.py
+++ messages/comment.py
@@ -1,6 +1,8 @@
-from typing import List
-from datetime import datetime
 from __future__ import annotations
+
+from datetime import datetime
+from typing import List
+
 
 class Comment(object):
     author: str
@@ -8,7 +10,9 @@ class Comment(object):
     created_on: datetime
     replies: List[Comment]
 
-    def __init__(self, author: str, body: str, created_on: datetime, replies: List[Comment]) -> None:
+    def __init__(
+        self, author: str, body: str, created_on: datetime, replies: List[Comment]
+    ) -> None:
         self.author = author
         self.body = body
         self.created_on = created_on
